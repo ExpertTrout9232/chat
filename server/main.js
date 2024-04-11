5827ac1ddf2fd298879815129d83c59728c81d86
KP <KP@PCFEIB208-86.msad.vsb.cz>
2024-04-11 17:04:54 +0200

    I just broke literally everything

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server(3000, { cors: { origin: '*' } });
 
+let message_count = 0;
 
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -12,7 +13,9 @@ io.on('connection', (socket) => {
 
   socket.on('chat_message', (msg) => {
     console.log('message: ' + msg);
-    socket.broadcast.emit('chat_message', msg);
+    let parsed_msg = JSON.parse(msg);
+    message_count++;
+    socket.broadcast.emit('chat_message', JSON.stringify({"message":parsed_msg.message,"message_index":message_count,"name":parsed_msg.name}));
   });
 });
 
