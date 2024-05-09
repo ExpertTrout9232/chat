524f224dc0bd2196156d2f6570a42d4656e7abf8
KP <KP@PCFEIB208-87.msad.vsb.cz>
2024-05-09 17:07:56 +0200

    fixed visuals, started adding rooms functions, currently not working

--- server/main.js
+++ server/main.js
@@ -2,8 +2,6 @@ import { Server } from "socket.io";
 
 const io = new Server(3000, { cors: { origin: '*' } });
 
-let message_count = 0;
-
 io.on('connection', (socket) => {
   console.log('A user connected');
 
@@ -13,9 +11,9 @@ io.on('connection', (socket) => {
 
   socket.on('chat_message', (msg) => {
     console.log('message: ' + msg);
-    let parsed_msg = JSON.parse(msg);
-    message_count++;
-    socket.broadcast.emit('chat_message', JSON.stringify({"message":parsed_msg.message,"message_index":message_count,"name":parsed_msg.name}));
+    msg = JSON.parse(msg);
+    socket.join(msg.room);
+    socket.to(room).emit('chat_message', msg);
   });
 });
 
